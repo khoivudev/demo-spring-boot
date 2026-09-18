196f1f620da8b25e0f5a065b73d7d8875e6b56f4
vhkhoi <khoi.vu0901@gmail.com>
2026-09-18 22:43:55 +0900

    inject custom properties from application.properties

--- src/main/java/com/khoivu/demo/rest/HelloWorldController.java
+++ src/main/java/com/khoivu/demo/rest/HelloWorldController.java
@@ -1,14 +1,18 @@
 package com.khoivu.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
 public class HelloWorldController {
 
+    // inject properties from application.properties file
+    @Value ("${author.name}")
+    private String authorName;
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World 123!";
+        return "Hello World, Author: " + authorName;
     }
 
 }
