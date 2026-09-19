ae07ae3207d513ad9eba1795081cb06ed4829e4f
vhkhoi <khoi.vu0901@gmail.com>
2026-09-19 10:58:21 +0900

    demo bean scopes

--- src/main/java/com/khoivu/demo/common/TrackCoach.java
+++ src/main/java/com/khoivu/demo/common/TrackCoach.java
@@ -1,10 +1,11 @@
 package com.khoivu.demo.common;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 
 @Component
-@Lazy
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TrackCoach implements Coach {
   public TrackCoach() {
     System.out.println(">> TrackCoach: inside default constructor");
--- src/main/java/com/khoivu/demo/rest/DemoController.java
+++ src/main/java/com/khoivu/demo/rest/DemoController.java
@@ -5,22 +5,32 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RestController;
 import com.khoivu.demo.common.Coach;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController 
 public class DemoController {
   private Coach coach;
 
+  private Coach anotherCoach;
 
   @Autowired 
-  public DemoController(@Qualifier("baseballCoach") Coach coach) {
+  public DemoController(@Qualifier("trackCoach") Coach coach, @Qualifier("trackCoach") Coach anotherCoach) {
     System.out.println(">> DemoController: inside constructor");
     this.coach = coach;
+    this.anotherCoach = anotherCoach;
   }
+
 
   @GetMapping("/dailyworkout")
   public String getDailyWorkout() {
       return coach.getDailyWorkout();
+  }
+  
+  @GetMapping("/check")
+  public String check() {
+      return "Compare the two coach instances: " + (coach == anotherCoach);
   }
   
 }
